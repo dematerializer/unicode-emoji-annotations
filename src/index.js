8f21f1f739065169d03beff3d3ca3243bb96f4f5
Andreas Tietz <andreas.tietz@gmail.com>
2016-12-21 12:53:07 +0100

    Export API the ES6 way using named exports

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
 import combineAnnotations from './combine-annotations';
 
-const combineAnnotationsForLanguage = (language) => {
-	const cldrAnnotations = require(`../res/cldr/stable/${language}.json`); // eslint-disable-line
+export const combinedAnnotationsForLanguage = (language) => {
+	const cldrAnnotations = require(`../res/cldr/${language}.json`); // eslint-disable-line
 	const communityAnnotations = require(`../res/community/${language}.json`); // eslint-disable-line
 	const globalCommunityAnnotations = require('../res/community/global.json'); // eslint-disable-line
 	return combineAnnotations(cldrAnnotations, communityAnnotations, globalCommunityAnnotations);
 };
 
-export default {
-	combined: combineAnnotationsForLanguage,
-	cldr: {
-		de: require('../res/cldr/de.json'), // eslint-disable-line global-require
-		en: require('../res/cldr/en.json'), // eslint-disable-line global-require
-	},
-	community: {
-		global: require('../res/community/global.json'), // eslint-disable-line global-require
-		de: require('../res/community/de.json'), // eslint-disable-line global-require
-		en: require('../res/community/en.json'), // eslint-disable-line global-require
-	},
+export const cldrAnnotations = {
+	de: require('../res/cldr/de.json'), // eslint-disable-line global-require
+	en: require('../res/cldr/en.json'), // eslint-disable-line global-require
+};
+
+export const communityAnnotations = {
+	global: require('../res/community/global.json'), // eslint-disable-line global-require
+	de: require('../res/community/de.json'), // eslint-disable-line global-require
+	en: require('../res/community/en.json'), // eslint-disable-line global-require
 };
