abfc0d0c8c1e70980963e2433ef4fb8fafb0ff6a
Andreas Tietz <andreas.tietz@gmail.com>
2017-05-03 00:30:02 +0200

    Put build scripts in a separate directory

--- scripts/build-cldr-annotations.js
+++ scripts/build-cldr-annotations.js
@@ -1,4 +1,4 @@
 require('log-update')('⌛︎ initializing');
 require('babel-register');
 require('babel-polyfill');
-require('./src/build-cldr-annotations');
+require('../src/build-cldr-annotations');
